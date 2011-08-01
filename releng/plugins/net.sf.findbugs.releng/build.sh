5e0bb02da1a48ba6290356031ca944f1cd83b26d
rhchen <ranhow.chen@gmail.com>
2011-08-02 01:01:13 +0800

    Add build setting reference
    	modified:   releng/plugins/net.sf.findbugs.releng/build.properties
    	modified:   releng/plugins/net.sf.findbugs.releng/build.sh

--- releng/plugins/net.sf.findbugs.releng/build.sh
+++ releng/plugins/net.sf.findbugs.releng/build.sh
@@ -9,10 +9,8 @@
 # Build for eclipse 3.7
 eclipseDist="/home/rhchen/Downloads/eclipse/eclipse/eclipse-SDK-3.7-linux-gtk.tar.gz"
 eclipseDeltaDist="/home/rhchen/Downloads/eclipse/eclipse/eclipse-3.7-delta-pack.zip"
-cdtDist="/home/rhchen/Downloads/eclipse/cdt/cdt-master-8.0.0-I201106081058.zip"
-rseDist="/home/rhchen/Downloads/eclipse/rse/RSE-SDK-3.3.zip"
 
-workDir="/home/rhchen/works/build/rse"
+workDir="/home/rhchen/works/build/findbugs"
 rm -Rf ${workDir}
 mkdir -p ${workDir}
 echo "workdir is ${workDir}"
@@ -37,16 +35,6 @@ echo "srcdir is ${srcDir}"
 cd ${baseLocation}
 tar -xzvf ${eclipseDist}
 
-unzip -o -d cdt ${cdtDist}
-cp -Rf cdt/plugins/* eclipse/plugins/
-cp -Rf cdt/features/* eclipse/features/
-rm -Rf cdt
-
-unzip -o -d rse ${rseDist}
-cp -Rf rse/eclipse/plugins/* eclipse/plugins/
-cp -Rf rse/eclipse/features/* eclipse/features/
-rm -Rf rse
-
 unzip -o -d delta ${eclipseDeltaDist}
 cp -Rf delta/eclipse/plugins/* eclipse/plugins/
 cp -Rf delta/eclipse/features/* eclipse/features/
@@ -56,7 +44,7 @@ rm -Rf delta
 echo "Fetch Source Codes from github ....."
 
 cd ${srcDir}
-git clone git://github.com/rhchen/RSE-EXT.git
+git clone git://github.com/rhchen/findbugs-rcp.git
 
 cp -Rf ${srcDir}/RSE-EXT/rse/* ${buildDirectory}
 cp -Rf ${srcDir}/RSE-EXT/releng/* ${buildDirectory}
@@ -75,7 +63,12 @@ echo "Start build RSE-EXT"
 #java -jar ${baseLocation}/eclipse/plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar -application org.eclipse.ant.core.antRunner -buildfile ${baseLocation}/eclipse/plugins/org.eclipse.pde.build_3.7.0.v20110512-1320/scripts/productBuild/productBuild.xml -Dbuilder=${buildConfiguration}
 
 # Feature Build For Eclipse 3.7
-java -jar ${baseLocation}/eclipse/plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar -application org.eclipse.ant.core.antRunner -buildfile ${baseLocation}/eclipse/plugins/org.eclipse.pde.build_3.7.0.v20110512-1320/scripts/build.xml -Dbuilder=${buildConfiguration}/
+java -jar ${baseLocation}/eclipse/plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar \
+	-application org.eclipse.ant.core.antRunner \
+	-buildfile ${baseLocation}/eclipse/plugins/org.eclipse.pde.build_3.7.0.v20110512-1320/scripts/build.xml \
+	-Dbuilder=${buildConfiguration}/ \
+	-DbuildDirectory=${buildDirectory} \
+	-Dbase=${baseLocation}
 
 # Feature Build for Eclipse 3.6.2
 #java -jar ${baseLocation}/eclipse/plugins/org.eclipse.equinox.launcher_1.1.1.R36x_v20101122_1400.jar -application org.eclipse.ant.core.antRunner -buildfile ${baseLocation}/eclipse/plugins/org.eclipse.pde.build_3.6.2.R36x_20110203/scripts/build.xml -Dbuilder=${buildConfiguration}/
